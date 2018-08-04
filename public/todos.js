ad2df16ca975900c5b2f80e1b7168a422e0025ed
Igor Atakhanov <igoratakhanov@gmail.com>
2018-08-04 18:48:27 -0400

    basic front end todos script done

--- public/todos.js
+++ public/todos.js
@@ -1,9 +1,77 @@
-(function(){
-  "use strict"
+(function() {
+  'use strict';
   /**
    * Initial get call initializes the script, if successful.
    */
   const state = {
-    todos:[]
-  }
-})()
+    todos: [],
+    get getTodos() {
+      return this.todos;
+    },
+    addTodo(todo) {
+      this.todos = [...this.todos, todo];
+    },
+    populateTodos(todos) {
+      this.todos = todos;
+    },
+    populateList(list, todos) {
+      todos.map(todo => {
+        list.insertAdjacentHTML('beforeend', `<li>${todo.todo}</li>`);
+      });
+    },
+    addToList(list, todo) {
+      console.log('adding:', todo);
+      list.insertAdjacentHTML('beforeend', `<li>${todo.todo}</li>`);
+    }
+  };
+  const ul = document.querySelector('#todos-list');
+  const form = document.querySelector('#todos-form');
+  const inputField = document.querySelector('#todos-field');
+
+  fetch('http://localhost:3000/todos/get', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  }).then(
+    res => {
+      console.log(res);
+      if (res.ok) {
+        res.json().then(
+          json => {
+            console.log(json);
+            state.populateTodos(json.todos);
+            state.populateList(ul, state.getTodos);
+            render();
+          },
+          err => console.log(err)
+        );
+      }
+    },
+    err => console.log(err)
+  );
+
+  const render = () => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      fetch('http://localhost:3000/todos/submit', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ todo: inputField.value })
+      })
+        .then(
+          res => {
+            return res.json();
+          },
+          err => console.log(err)
+        )
+        .then(json => {
+          state.addTodo(json);
+          state.addToList(ul, json);
+        });
+    });
+  };
+})();
--- src/app.js
+++ src/app.js
@@ -1,56 +1,55 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const session = require("express-session");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
-const { signUp, logIn } = require("./libs/user/controllers");
-const { submitTodo, getTodos } = require("./libs/todos/controllers");
+const { signUp, logIn } = require('./libs/user/controllers');
+const { submitTodo, getTodos } = require('./libs/todos/controllers');
 
 const app = express();
-const isLoggedIn = require("./libs/middleware/isLoggedIn");
+const isLoggedIn = require('./libs/middleware/isLoggedIn');
 
-const pathTo = fileName => path.resolve(__dirname, "..", "public", fileName);
+const pathTo = fileName => path.resolve(__dirname, '..', 'public', fileName);
 
 // const jsonParser = bodyParser.json();
 // const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(
   session({
-    secret: "our little s...",
+    secret: 'our little s...',
     resave: false,
     saveUninitialized: true
   })
 );
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-app.use(express.static(path.resolve(__dirname, "..", "public")));
-app.get("/", (req, res) => {
-  res.sendFile(pathTo("index.html"));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(pathTo('index.html'));
 });
-app.get("/user/login", (req, res) => {
-  res.sendFile(pathTo("login.html"));
+app.get('/user/login', (req, res) => {
+  res.sendFile(pathTo('login.html'));
 });
-app.get("/user/signup", (req, res) => {
-  res.sendFile(pathTo("register.html"));
+app.get('/user/signup', (req, res) => {
+  res.sendFile(pathTo('register.html'));
 });
-app.post("/user/signup", /*urlencodedParser,*/ signUp);
-app.post("/user/login", /*urlencodedParser,*/ logIn);
-app.post("/todos/submit", /*jsonParser,*/ isLoggedIn, submitTodo);
-app.get("/todos/get", /*jsonParser,*/ isLoggedIn, getTodos);
-app.get("/user/logout", async (req, res) => {
+app.post('/user/signup', /*urlencodedParser,*/ signUp);
+app.post('/user/login', /*urlencodedParser,*/ logIn);
+app.post('/todos/submit', /*jsonParser,*/ isLoggedIn, submitTodo);
+app.get('/todos/get', /*jsonParser,*/ isLoggedIn, getTodos);
+app.get('/user/logout', async (req, res) => {
   await req.session.destroy();
-  res.redirect("/");
+  res.redirect('/');
 });
 
-app.get("/todos", (req, res) => {
-  res.sendFile(pathTo("todos.html"));
+app.get('/todos', isLoggedIn, (req, res) => {
+  res.sendFile(pathTo('todos.html'));
 });
 
-app.get("/401", (req, res) => {
-  res.sendFile(pathTo("401.html"));
+app.get('/401', (req, res) => {
+  res.sendFile(pathTo('401.html'));
 });
 
 module.exports = app;
