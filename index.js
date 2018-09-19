cd464273abc179f8d68d90602f86b5c710103405
Igor Atakhanov <igoratakhanov@gmail.com>
2018-09-19 00:18:43 -0400

    needs to serve bundle, needs proxy for front end api calls

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const app = require('./src/app');
-const PORT = 3000;
+
 const mongoose = require('mongoose');
 
 const devDB = `mongodb://localhost/todos_tdd_dev`;
+
+const prodDB =
+  'mongodb://george123:jungle123@ds161322.mlab.com:61322/todos_tdd_prod';
 
 mongoose.connect(
   devDB,
   () => console.log(`connected to ${devDB}`)
 );
 
-app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`listening on http://localhost:3000`)
+);
